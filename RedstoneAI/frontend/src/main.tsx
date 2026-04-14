4465bac4c91eea276a239ac6a265d377d0ea6740
Jack Warder <jwarder01@gmail.com>
2026-04-13 23:04:01 -0600

    Analytics

--- RedstoneAI/frontend/src/main.tsx
+++ RedstoneAI/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App";
 
@@ -21,6 +22,7 @@ createRoot(document.getElementById("root")!).render(
       }}
     >
       <App />
+      <Analytics />
     </ClerkProvider>
   </StrictMode>
 );
